2820b05ec1005ed2e854ce70af8cc535394ff4bd
Rayyslay <rayan.elhajj.06@gmail.com>
2026-01-30 14:05:35 +0200

    Fresh Restart
    Have this directory for the fullstack website

--- testSite1/Data/AppDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace testSite1.Data;
-
-public class AppDbContext : DbContext {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
-    }
-}
